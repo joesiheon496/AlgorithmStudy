dcf3cf299c1216a3e44c27952dc6dfbe9bf58022
조시헌 <56191064+joesiheon496@users.noreply.github.com>
2022-09-28 14:32:12 +0900

    Update Linked_List.py

--- DataStructure/Linked_List.py
+++ DataStructure/Linked_List.py
@@ -2,12 +2,22 @@ class Node:
     def __init__(self,data,next=None):
         self.data = data
         self.next = next
-
-def add(data):
-    # 링크드 리스트의 시작점 지정
-    node = head
-    # 마지막 노드를 찾기위한 작업
-    while node.next:
-        node = node.next
-    # 새로운 노드 추가, 연결
-    node.next = Node(data)
+        
+class NodeMerge:
+    def __init__(self,data):
+        self.head = Node(data)
+    
+    def add(self,data):
+        if self.head == '':
+            self.head = Node(data)
+        else:
+            node = self.head
+            while node:
+                node = node.next
+            node.next = Node(data)
+            
+    def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
