f294d8c473798d5577eb7408bd53ee642b5e2f8d
조시헌 <56191064+joesiheon496@users.noreply.github.com>
2022-09-28 13:41:13 +0900

    Add files via upload

---
+++ DataStructure/Linked_List.py
@@ -0,0 +1,16 @@
+from email import header
+
+
+class Node:
+    def __init__(self,data,next=None):
+        self.data = data
+        self.next = next
+
+def add(data):
+    # 링크드 리스트의 시작점 지정
+    node = head
+    # 마지막 노드를 찾기위한 작업
+    while node.next:
+        node = node.next
+    # 새로운 노드 추가, 연결
+    node.next = Node(data)
