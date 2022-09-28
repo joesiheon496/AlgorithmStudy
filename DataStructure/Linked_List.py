93707dbd925f1a1a666418701e83da6b837e67ae
조시헌 <56191064+joesiheon496@users.noreply.github.com>
2022-09-28 14:46:40 +0900

    Update Linked_List.py

--- DataStructure/Linked_List.py
+++ DataStructure/Linked_List.py
@@ -5,13 +5,16 @@ class Node:
         
 class NodeMerge:
     def __init__(self,data):
+        # Class 호출과 동시에 시작 노드 생성
         self.head = Node(data)
     
     def add(self,data):
+        # 혹시 방어 코드
         if self.head == '':
             self.head = Node(data)
         else:
             node = self.head
+            # node.next가 None이면 이어지는 노드가 없으니 새로 추가하는 노드 추가
             while node:
                 node = node.next
             node.next = Node(data)
