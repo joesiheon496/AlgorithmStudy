e2262771c726ea94a32c529728fedce5eca19da8
조시헌 <56191064+joesiheon496@users.noreply.github.com>
2022-09-28 15:31:23 +0900

    Update DoubleLinkedList.py

--- DataStructure/DoubleLinkedList.py
+++ DataStructure/DoubleLinkedList.py
@@ -28,10 +28,10 @@ class NodeMgmt:
       print(node.data)
       node = node.next
       
-   def sear_from_head(self,data):
+  def sear_from_head(self,data):
     if self.head == None:
       return False
-    
+
     node = self.head
     while node:
       if node.data == data:
@@ -39,3 +39,33 @@ class NodeMgmt:
       else:
         node = node.next
     return False
+
+  def sear_from_tail(self,data):
+    if self.head == None:
+      return False
+
+    node = self.tail
+    while node:
+      if node.data == data:
+        return node
+      else:
+        node = node.prev
+    return False
+  
+  def insert_before(self,data,before_data):
+    if self.head == None:
+      self.head = Node(data)
+      return True
+    else:
+      node = self.tail
+      while node.data != before_data:
+        node = node.prev
+        if node == None:
+          return False
+      new = Node(data)
+      before_new = node.prev
+      before_new.next = new
+      new.prev = before_new
+      new.next = node
+      node.prev = new
+      return True
